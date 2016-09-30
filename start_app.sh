a9e8c87046836a94b7bbd91a8f1a9baa6587f34b
Ubuntu <ubuntu@ip-172-31-30-25.ap-southeast-2.compute.internal>
2016-09-30 12:57:38 +0000

    Updated start script

--- start_app.sh
+++ start_app.sh
@@ -3,7 +3,7 @@
 # Invoke the Forever module (to START our Node.js server).
 ./node_modules/forever/bin/forever \
 start \
--al logs/forever.log \
--ao logs/out.log \
--ae logs/err.log \
-app.js
+-al forever.log \
+-ao out.log \
+-ae err.log \
+./bin/www
